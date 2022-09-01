e4cb6cb6b96cf14b085c48407d9c618fe5890e01
chavda-bhavik <bhavikvchavda@gmail.com>
2022-09-01 09:52:54 +0530

    updated image to check for large attachment issue

--- router.js
+++ router.js
@@ -4,51 +4,52 @@ const path = require('path');
 const fs = require('fs');
 const { Novu } = require('@novu/node')
 
-const API_KEY = '...';
-const TO_EMAIL = '...';
-const TO_PHONE = '...';
-const IMG_PATH = path.join(__dirname, 'img/facebook.png')
-const SUBSCRIBER_ID = '1234';
-const NOTIFICATION_EVENT_ID = 'test';
+const API_KEY = "";
+const TO_EMAIL = "";
+const TO_PHONE = "";
+const IMG_PATH = path.join(__dirname, "img/img-large.jpg");
+const SUBSCRIBER_ID = "1234";
+const NOTIFICATION_EVENT_ID = "test";
 
 const novuInstance = new Novu(API_KEY);
 
 // For every student
-router.post('/register', async (req, res) => {
-    try {
-        let response = await novuInstance.subscribers.identify(SUBSCRIBER_ID, {
-            firstName: "John",
-            lastName: "Doe",
-            email: TO_EMAIL,
-            phone: TO_PHONE
-        });
-        res.status(200).send(response.data)
-    } catch (error) {
-        res.status(400).send({ message: error.message })
-    }
-})
+router.post("/register", async (req, res) => {
+  try {
+    let response = await novuInstance.subscribers.identify(SUBSCRIBER_ID, {
+      firstName: "John",
+      lastName: "Doe",
+      email: TO_EMAIL,
+      phone: TO_PHONE,
+    });
+    res.status(200).send(response.data);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
 
 // Sending Email
-router.post('/send', async (req, res) => {
-    try {
-        let response = await novuInstance.trigger(NOTIFICATION_EVENT_ID, {
-            to: {
-                subscriberId: SUBSCRIBER_ID
-            },
-            payload: {
-                name: "John",
-                attachments: [{
-                    file: fs.readFileSync(IMG_PATH),
-                    name: 'facebook.png',
-                    mime: 'image/png',
-                }]
-            }
-        });
-        res.status(200).send(response.data);
-    } catch (error) {
-        res.status(400).send({ message: error.message });
-    }
-
-})
+router.post("/send", async (req, res) => {
+  try {
+    let response = await novuInstance.trigger(NOTIFICATION_EVENT_ID, {
+      to: {
+        subscriberId: SUBSCRIBER_ID,
+      },
+      payload: {
+        name: "John",
+        attachments: [
+          {
+            file: fs.readFileSync(IMG_PATH),
+            name: "img-large.jpg",
+            mime: "image/jpg",
+          },
+        ],
+      },
+    });
+    res.status(200).send(response.data);
+  } catch (error) {
+    res.status(400).send({ message: error.message });
+  }
+});
 
 module.exports = router
